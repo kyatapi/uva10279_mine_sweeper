5e6ac11366429d15ee370d6d9bca4be91326ad3b
mike_su <mike_su@nuuo.com>
2017-08-05 22:56:57 +0800

    Commit first raft with basic logic

---
+++ mine_sweeper.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+void test_case() {
+}
+
+int main() {
+    size_t case_count = 0;
+    cin >> case_count;
+    for(size_t c = 0; c < case_count; ++c) test_case();
+}
